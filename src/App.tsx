d42fad9856f9f5d74dba889dd2b3314007120715
dmuchai <dmmuchai@gmail.com>
2025-07-08 19:38:16 +0300

    feat: add HomePage and AgentDashboardPage mock components with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AgentDashboardPage from './pages/AgentDashboardPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard/agent" element={<AgentDashboardPage />} />
+        <Route path="/dashboard" element={<AgentDashboardPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
 export default App;
